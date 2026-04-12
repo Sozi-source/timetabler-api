0f3c95790bf74e2b7e2c9f958f928e2ed2b27e1d
Wilfred Osozi <osoziw@gmail.com>
2026-04-12 23:10:21 +0300

    Redirect root to swagger

--- timetabler/urls.py
+++ timetabler/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/'), name='root'),
+
     path('admin/', admin.site.urls),
     path('api/', include('timetable.urls')),  # Your timetable app URLs
 
