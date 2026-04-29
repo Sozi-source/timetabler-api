0c5c0dc5ca8b2c9fecb2bdf5f0a276db3b45cb4e
Wilfred Osozi <osoziw@gmail.com>
2026-04-29 11:19:47 +0300

    add production setup script

---
+++ setup_prod.py
@@ -0,0 +1,9 @@
+﻿from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+u, _ = User.objects.get_or_create(username='admin')
+u.set_password('admin123')
+u.is_staff = True
+u.is_superuser = True
+u.save()
+t, _ = Token.objects.get_or_create(user=u)
+print('PRODUCTION TOKEN:', t.key)
