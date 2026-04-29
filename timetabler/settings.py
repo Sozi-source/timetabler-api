f9e083373daae636c1488b4d875e11ba1b6e3fb1
Wilfred Osozi <osoziw@gmail.com>
2026-04-29 11:38:58 +0300

    fix: restore REST_FRAMEWORK settings with TokenAuthentication

--- timetabler/settings.py
+++ timetabler/settings.py
@@ -225,3 +225,26 @@ LOGGING = {
 }
 
 
+
+# --- Django REST Framework ---------------------------------------------------
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "EXCEPTION_HANDLER": "timetable.exceptions.custom_exception_handler",
+}
